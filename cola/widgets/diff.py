174daedd795ab23752792c548daba4ec977218e9
Daniel Harding <dharding@living180.net>
2022-01-24 17:12:00 +0200

    WIP

--- cola/widgets/diff.py
+++ cola/widgets/diff.py
@@ -710,7 +710,6 @@ class DiffEditor(DiffTextEdit):
     up = Signal()
     down = Signal()
     options_changed = Signal()
-    updated = Signal()
     diff_text_updated = Signal(object)
 
     def __init__(self, context, options, parent):
@@ -749,11 +748,9 @@ class DiffEditor(DiffTextEdit):
         model.add_observer(diff_text_updated, self.diff_text_updated.emit)
         self.diff_text_updated.connect(self.set_diff, type=Qt.QueuedConnection)
 
-        selection_model.add_observer(
-            selection_model.message_selection_changed, self.updated.emit
+        selection_model.selection_changed.connect(
+            self.refresh, type=Qt.QueuedConnection,
         )
-        # pylint: disable=no-member
-        self.updated.connect(self.refresh, type=Qt.QueuedConnection)
         # Update the selection model when the cursor changes
         self.cursorPositionChanged.connect(self._update_line_number)
 
